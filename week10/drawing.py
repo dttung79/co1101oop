47c6ef6c040d3b94a766d19b444f974247e29a3b
dttung79 <dttung@gmail.com>
2023-07-26 09:00:47 +0700

    Solution for drawing_ex

--- week10/drawing.py
+++ week10/drawing.py
@@ -11,6 +11,8 @@ class Game:
         self.RED = (255, 0, 0)
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
+        self.CYAN = (0, 255, 255)
+        self.YELLOW = (255, 255, 0)
         
     def draw_rectangle(self, position, size):
         # Draw a red rectangle
@@ -24,10 +26,18 @@ class Game:
         # Draw a blue line
         pygame.draw.line(self.window, self.BLUE, start_position, end_position, width)
 
+    def draw_triangle(self, positions):
+        pygame.draw.polygon(self.window, self.YELLOW, positions)
+
+    def draw_ellipse(self, rect):
+        pygame.draw.ellipse(self.window, self.CYAN, rect)
+
     def draw_shapes(self):
         self.draw_rectangle((50, 50), (200, 100))
         self.draw_circle((400, 300), 50)
         self.draw_line((600, 100), (700, 500), 5)
+        self.draw_triangle(((100, 100), (200, 100), (150, 200)))
+        self.draw_ellipse((500, 300, 400, 100))
 
     def run(self):
         while True:
---
+++ week10/drawing_ex.py
@@ -0,0 +1,41 @@
+# Exercise: Pygame Shape Drawing
+
+# Your task is to extend the basic Pygame application provided in class. Here are your instructions:
+
+# Create a Game class similar to the one provided in class. Define it in such a way that when you create an instance of the class, you can specify the window's width and height.
+
+# Define color variables for YELLOW, PURPLE, and CYAN within the Game class.
+
+# Create separate methods for drawing a triangle, an ellipse, and a polygon. Each of these methods should accept parameters for the shape's position and size. The color for each shape should be one of the colors defined in step 2.
+
+# For the triangle, consider using the pygame.draw.polygon function, which can draw any polygon, not just triangles. You just need to provide three points for the vertices of the triangle.
+
+# For the ellipse, use the pygame.draw.ellipse function. Keep in mind that the position and size parameters for this function specify the rectangle within which the ellipse is drawn.
+
+# For the polygon, you can also use the pygame.draw.polygon function. Try to draw a pentagon or hexagon.
+
+# Create a draw_shapes method that calls each of the shape-drawing methods with appropriate arguments to draw the shapes at different positions and with different sizes.
+
+# Finally, create a run method that starts the game loop. In each frame of the loop, the window should be filled with a color of your choice, the shapes should be drawn by calling the draw_shapes method, and the display should be updated.
+
+# Outside of the Game class, create an instance of the class and call its run method to start the game.
+
+# Remember to handle the QUIT event in your game loop to allow the user to close the game window.
+import pygame
+import sys
+pygame.init()
+
+w = pygame.display.set_mode((800, 600))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    w.fill((0, 0, 0))
+    pygame.draw.polygon(w, (255, 255, 0), 
+                           ((100, 100), 
+                           (200, 100), 
+                           (150, 200),
+                           (50, 200)))
+    pygame.draw.ellipse(w, (255, 0, 255), (300, 300, 200, 100))
+    pygame.display.flip()
